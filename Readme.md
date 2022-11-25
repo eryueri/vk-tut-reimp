aac57380bee67b178187d7f22b7d40f8a25598c5
eryueri <786684105@qq.com>
2022-11-25 23:45:13 +0800

    add Readme

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# vk-tut-reimp
+
+implementation of mine of [this tutorial](https://vulkan-tutorial.com)
+
+structures, namings and some details are different because of personal habbits
+
+not very good at cpp coding, learning slowly
---
+++ include/ImageRenderer.hh
@@ -0,0 +1,12 @@
+
+class VulkanInstance;
+class RenderAssets;
+
+class ImageRenderer {
+public:
+  ImageRenderer() = default;
+  ~ImageRenderer() = default;
+  void init(VulkanInstance* instance, RenderAssets* assets);
+private:
+
+};
